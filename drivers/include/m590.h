ebfdcc4c8a2e77b014bb4a9ebc35c115fdbbf5a9
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-07-28 16:34:42 +0200

    WIP

---
+++ drivers/include/m590.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014-2016 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    drivers_periph_spi SPI
+ * @ingroup     drivers_periph
+ * @brief       Low-level SPI peripheral driver
+ *
+ * @{
+ * @file
+ * @brief       Low-level SPI peripheral driver interface definition
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef DRIVER_M590_H
+#define DRIVER_M590_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DRIVER_M590_H */
+/** @} */
